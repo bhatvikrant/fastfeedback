1dc87ed3d66cc2cef9d66d41265c44a7a5651cc9
Vikrant Bhat <vik.bhat223@gmail.com>
2021-02-08 16:58:11 +0530

    Icons setup compelete

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,35 @@
 import { useAuth } from '@/lib/auth';
+import { LogoIcon } from '@/styles/customIcons';
 
-import { Heading, Text, Code, Button } from '@chakra-ui/react';
+import { Heading, Text, Code, Button, Flex } from '@chakra-ui/react';
 
 const Home = () => {
     const auth = useAuth();
     return (
-        <div>
-            <main>
-                <Heading>Fast Feedback</Heading>
-                <Text>
-                    Current user:{' '}
-                    <Code>{auth.user ? auth.user.email : 'None'}</Code>
-                </Text>
-                {auth.user ? (
-                    <Button onClick={() => auth.signout()}>sign out</Button>
-                ) : (
-                    <Button onClick={() => auth.signinWithGitHub()}>
-                        sign in
-                    </Button>
-                )}
-            </main>
-        </div>
+        <Flex
+            as="main"
+            direction="column"
+            align="center"
+            justifyContent="center"
+            h="100vh"
+        >
+            <LogoIcon color="black" boxSize="64px" />
+            {/* <Text>
+                Current user:{' '}
+                <Code>{auth.user ? auth.user.email : 'None'}</Code>
+            </Text> */}
+            {auth.user ? (
+                <Button onClick={() => auth.signout()}>sign out</Button>
+            ) : (
+                <Button
+                    mt={4}
+                    size="sm"
+                    onClick={() => auth.signinWithGitHub()}
+                >
+                    sign in
+                </Button>
+            )}
+        </Flex>
     );
 };
 export default Home;
